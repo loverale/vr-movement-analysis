5cd27a330d2130bfb933d48f4ea7097d7cff811f
alex <loverale@msu.edu>
2024-10-22 00:08:29 -0400

    iteration of all files for gaze, tested, works

--- VR_Movement_M22/.venv/movement_script.py
+++ VR_Movement_M22/.venv/movement_script.py
@@ -4,6 +4,9 @@ import os
 import csv
 import warnings
 
+gaze_towards_instructor = 0
+folder_path = './master'  # REPLACE WITH FILE PATH THIS JUST HAPPENS TO BE MY PATH IN THE VIRTUAL ENVIRONMENT
+
 # setup instructions:
 # pip install pandas numpy os csv warning
 
@@ -37,8 +40,7 @@ def vector_from_rotation(rotation):
 # that field of view.
 # fairly barbaric calculation, as it doesn't necessarily account for distance
 # function returns 1 for each line calculation, goal is to divide by 30 to get seconds looked at prof
-def calculate_gaze_towards_instructor(student_pos, student_rotation, instructor_pos, threshold=0.9)
-    print("Calculating Gaze")
+def calculate_gaze_towards_instructor(student_pos, student_rotation, instructor_pos, threshold=0.9):
     # Get the gaze direction of student
     student_gaze_direction = vector_from_rotation(student_rotation)
 
@@ -63,17 +65,15 @@ def calculate_gaze_towards_instructor(student_pos, student_rotation, instructor_
 # this script will calculate delta in 30 frame interval (1 second)
 # i do this instead of frame by frame as every student will move a similar speed frame by frame
 # but the entire second will better differentiate movements between students
-def calculate_deltas_in_intervals(df, cols, interval=30):
+def calculate_deltas_in_intervals(df, cols):
     # initialization to ensure empty df per file
     deltas = []
     num_rows = len(df)
 
     # does the thing
-    for i in range(0, num_rows, interval):
-        end_index = min(i + interval, num_rows) - 1  # ensures non-30 frame intervals are covered
-        if end_index > i:  # valid interval alert
-            delta = df.loc[end_index, cols].values - df.loc[i, cols].values
-            deltas.append(np.abs(delta))  # Store the absolute delta
+    for i in range(0, num_rows):
+        delta = df.loc[i+1, cols].values - df.loc[i, cols].values
+        deltas.append(np.abs(delta))  # Store the absolute delta
 
     return np.array(deltas)  # np has dope functions
 
@@ -128,6 +128,67 @@ def delta_process_file(file_path):
 
     return result_df
 
+def gaze_process_file(file_path, pPROF):
+    # bringing global variable, c# habits die hard
+    global gaze_towards_instructor
+
+    # temp variables parsing file name
+    participant_id = os.path.basename(file_path).split('.')[
+        0]  # remember to rename files later
+    class_no = os.path.basename(file_path).split('c')[
+        1]  # remember to rename files later
+
+    #print(class_no)
+
+    correct_prof = [file for file in pPROF if class_no in file]
+    global folder_path
+    correct_prof = os.path.join(folder_path, correct_prof[0])
+
+    prof_data = pd.read_csv(correct_prof, delim_whitespace=True)
+
+    stu_data = pd.read_csv(file_path, delim_whitespace=True)
+    #prof_data = pd.read_csv(correct_prof, delim_whitespace=True) # now checked by prveious if function
+    prof_data.set_index('full_frame_no', inplace=True) # sets index for faster checking
+
+    # define columns for head and hand positions
+    frame_number = ['full_frame_no']
+    head_position = ['HeadPosition_x', 'HeadPosition_y', 'HeadPosition_z']
+    head_rotation = ['HeadRotation_x', 'HeadRotation_y', 'HeadRotation_z']
+
+    rows_to_iterate = ['full_frame_no', 'HeadPosition_x', 'HeadPosition_y', 'HeadPosition_z', 'HeadRotation_x', 'HeadRotation_y', 'HeadRotation_z']
+
+    # temp copy delete later
+    # frame_number, head_position, head_rotation
+    for i, row in stu_data[rows_to_iterate].iterrows():
+        row_number = row['full_frame_no']
+        stu_pos = row['HeadPosition_x'], row['HeadPosition_y'], row['HeadPosition_z']
+        stu_rot = row['HeadRotation_x'], row['HeadRotation_y'] # gaze only needs x, y, since roll (direction of z) can be inferred by sin(pitch)
+
+        if row_number in prof_data.index:
+            corresponding_row = prof_data.loc[row_number]
+            ins_pos = corresponding_row['HeadPosition_x'], corresponding_row['HeadPosition_y'], corresponding_row[
+                'HeadPosition_z']
+            gaze_towards_instructor += calculate_gaze_towards_instructor(stu_pos, stu_rot, ins_pos)
+        else:
+            # print(f"    No match found for frame, skipping") # this was clogging the terminal, couldnt track by eye progress
+            continue
+
+    #length = len(file_path)
+    student_total_time = (stu_data['full_frame_no'].iloc[-1] - stu_data['full_frame_no'].iloc[0]) / 30
+    student_total_time = round(student_total_time, 2) # rounding to 2 for easy viewing
+    gaze_towards_instructor = gaze_towards_instructor / 30
+    gaze_towards_instructor = round(gaze_towards_instructor, 2)
+
+
+    # prepare the output
+    result_df = pd.DataFrame({
+        'participant_id': [participant_id],
+        'gazeins': [gaze_towards_instructor],
+        'total_time': [student_total_time]
+    })
+
+    return result_df
+
 # proving my insanity
 # not really needed for final computations. was just for descriptive
 def row_counter(file_path):
@@ -137,7 +198,7 @@ def row_counter(file_path):
     return row_count
 
 # this function goes through each folder in the directory and runs the functions
-def process_all_files_in_folder(folder_path):
+def delta_all_files_in_folder(folder_path):
     # iterates through all the files and starts the path through other functions
 
     all_results = pd.DataFrame()  # master file master file
@@ -160,6 +221,134 @@ def process_all_files_in_folder(folder_path):
     print(f"All processed data saved to {output_file}")
     print(f"Row count: {row_count}")
 
+# process_all_files_in_folder, but for gaze not delta
+def gaze_process_all_files(folder_path):
+    # iterates through all the files and starts the path through other functions
+
+    all_results = pd.DataFrame()  # master file master file
+    row_count = 0
+
+    p1 = []
+    p3 = []
+    p4 = []
+    p5 = []
+    p6 = []
+    p7 = []
+    p8 = []
+    p9 = []
+    p10 = []
+    p11 = []
+    p12 = []
+    p14 = []
+    p15 = []
+    p16 = []
+    p17 = []
+    p18 = []
+    p19 = []
+    p20 = []
+    p21 = []
+    p22 = []
+    p24 = []
+    p25 = []
+    p26 = []
+    p27 = []
+    p28 = []
+    p29 = []
+    p30 = []
+    p31 = []
+    p34 = []
+    p35 = []
+    pPROF = []
+
+    # does the thing
+    for file_name in os.listdir(folder_path):
+
+        # _ added to p1 and p3 to avoid adding files from p10, p30, etc
+        if("p1_" in file_name):
+            p1.append(file_name)
+        if("p3_" in file_name):
+            p3.append(file_name)
+        if ("p4" in file_name):
+            p4.append(file_name)
+        if("p5" in file_name):
+            p5.append(file_name)
+        if("p6" in file_name):
+            p6.append(file_name)
+        if("p7" in file_name):
+            p7.append(file_name)
+        if("p8" in file_name):
+            p8.append(file_name)
+        if("p9" in file_name):
+            p9.append(file_name)
+        if("p10" in file_name):
+            p10.append(file_name)
+        if("p11" in file_name):
+            p11.append(file_name)
+        if("p12" in file_name):
+            p12.append(file_name)
+        if("p14" in file_name):
+            p14.append(file_name)
+        if("p15" in file_name):
+            p15.append(file_name)
+        if("p16" in file_name):
+            p16.append(file_name)
+        if ("p17" in file_name):
+            p17.append(file_name)
+        if("p18" in file_name):
+            p18.append(file_name)
+        if("p19" in file_name):
+            p19.append(file_name)
+        if("p20" in file_name):
+            p20.append(file_name)
+        if("p21" in file_name):
+            p21.append(file_name)
+        if("p22" in file_name):
+            p22.append(file_name)
+        if("p24" in file_name):
+            p24.append(file_name)
+        if("p25" in file_name):
+            p25.append(file_name)
+        if("p26" in file_name):
+            p26.append(file_name)
+        if("p27" in file_name):
+            p27.append(file_name)
+        if("p28" in file_name):
+            p28.append(file_name)
+        if("p29" in file_name):
+            p29.append(file_name)
+        if("p30" in file_name):
+            p30.append(file_name)
+        if("p31" in file_name):
+            p31.append(file_name)
+        if("p34" in file_name):
+            p34.append(file_name)
+        if("p35" in file_name):
+            p35.append(file_name)
+        if("pPROF" in file_name):
+            pPROF.append(file_name)
+    all_files = [p1, p3, p4, p5, p6, p7, p8, p9, p10, p11, p12, p14, p15, p16, p17, p18, p19, p20, p21, p22, p24, p25, p26, p27, p28, p29, p30, p31, p34, p35]
+
+
+    for file_name in all_files:
+        for sub_file in file_name:
+            # .tsv check
+            if sub_file.endswith('.tsv'):
+                print(f"processing {sub_file}")
+                file_path = os.path.join(folder_path, sub_file)
+                with open(file_path, 'r') as f:
+                    row_count = row_count + sum(1 for line in f) - 1 # -1 for headers
+                result_df = gaze_process_file(file_path, pPROF)
+                global gaze_towards_instructor
+                gaze_towards_instructor = 0 # re-initialize
+                all_results = pd.concat([all_results, result_df], ignore_index=True)
+
+    # master file master file
+    output_file = os.path.join(folder_path, 'all_participants_gaze.csv')
+    all_results.to_csv(output_file, index=False)
+    print(f"    All processed data saved to {output_file}")
+    print(f"    Row count: {row_count}")
+
+
 # actual program that runs the things defined above
 if __name__ == "__main__":
     # never replicate this line of code
@@ -167,5 +356,9 @@ if __name__ == "__main__":
     warnings.filterwarnings("ignore", category=FutureWarning)
     # end of bad practices
 
-    folder_path = './day_master'  # REPLACE WITH FILE PATH THIS JUST HAPPENS TO BE MY PATH IN THE VIRTUAL ENVIRONMENT
-    process_all_files_in_folder(folder_path) # starts the thing
+    #global folder_path
+    print("PROCESSING DELTA STARTED")
+    #delta_all_files_in_folder(folder_path) # starts the thing
+
+    print("PROCESSING STU-INS GAZE")
+    gaze_process_all_files(folder_path)
